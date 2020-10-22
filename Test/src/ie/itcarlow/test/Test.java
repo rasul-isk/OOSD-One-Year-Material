f8b4628faf427a5695bb550ac32350c2baa97d75
rasul <rasul@LAPTOP-K1VMQB3G>
2020-10-22 15:56:43 +0000

    Added a third print statement

--- Test/src/ie/itcarlow/test/Test.java
+++ Test/src/ie/itcarlow/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
  public static void main(String[] args) {
 	 System.out.println("This is a test");
 	 System.out.println("This is a second print statement");
+	 System.out.println("This is a third print statement");
  }
 }
