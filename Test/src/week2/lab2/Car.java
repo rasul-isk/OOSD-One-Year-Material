d200309e43d751469493d35aa73d7f3c3d0b571d
rasul <rasul@LAPTOP-K1VMQB3G>
2020-10-22 21:26:43 +0000

    Created Car class and added constructors, getters, setters, toString

---
+++ Test/src/week2/lab2/Car.java
@@ -0,0 +1,84 @@
+package week2.lab2;
+
+public class Car {
+
+	///Variables
+	private String make;
+	private String model;
+	private double engineSize;
+	private double price;
+	private boolean sold;
+	
+	
+	///Constructors
+	public Car() {
+		setMake("");
+		setModel("");
+		setEngineSize(0.0);
+		setPrice(0.0);
+	
+	}
+	
+	public Car(String make, String model, double engineSize, double price) {
+		setMake(make);
+		setModel(model);
+		setEngineSize(engineSize);
+		setPrice(price);
+	}
+	
+	
+	///Getters & Setters
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public double getEngineSize() {
+		return engineSize;
+	}
+
+	public void setEngineSize(double engineSize) {
+		this.engineSize = engineSize;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public boolean isSold() {
+		return sold;
+	}
+
+	public void setSold(boolean sold) {
+		this.sold = sold;
+	}
+	
+	
+	//toString
+	@Override
+	public String toString() {
+		return "Car [make=" + make + ", model=" + model + ", engineSize=" + engineSize + ", price=" + price + ", sold="
+				+ sold + "]";
+	}
+	
+	
+	
+
+	
+
+}
