aede19271a93e815655a2ce4bfe1af160bfcaae6
rasul <rasul@LAPTOP-K1VMQB3G>
2020-10-26 17:44:54 +0000

    Created class Rectangle and added constructor, getter setter and variables

---
+++ Test/src/week3/lab1/Rectangle.java
@@ -0,0 +1,55 @@
+package week3.lab1;
+
+public class Rectangle {
+	
+	//Variables
+	private float length; 
+	private float width;
+	
+	
+	
+	
+	//Constructors
+	
+	public Rectangle() {
+		setLength(1.0f);
+		setWidth(1.0f);
+	}
+	
+	
+	
+	///Getter & Setters
+	
+	
+	public float getLength() {
+		return length;
+	}
+
+	public void setLength(float length) {
+		if(length > 0.0 && length <= 40.0) {
+			this.length = length;
+		}
+		else {
+			System.out.println("Length must be between 0.0 and 40.0");
+		}
+	}
+	public float getWidth() {
+		
+		return width;
+	}
+	public void setWidth(float width) {
+		if(length > 0.0 && length <= 40.0) {
+			this.width = width;
+		}
+		else {
+			System.out.println("Width must be between 0.0 and 40.0");
+		}
+		
+	}
+
+	
+	
+	//toString
+	
+
+}
