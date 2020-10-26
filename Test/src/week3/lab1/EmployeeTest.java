cd743d15f5657650551edd99196e44c1995dfd58
rasul <rasul@LAPTOP-K1VMQB3G>
2020-10-26 18:51:29 +0000

    Added Employee & EmployeeTest class

--- Test/src/week3/lab1/EmployeeTest.java
+++ Test/src/week3/lab1/EmployeeTest.java
@@ -10,10 +10,22 @@ public class EmployeeTest {
 		System.out.println("Employees after instantiation : " + Employee.getCount());
 		System.out.println("Employee 1 : " + e1.getFname() + " " + e1.getLname());
 		System.out.println("Employee 2 : " + e2);
+		
+		
+		Employee e3 = new Employee("Tom", "Jones");
+		Employee e4 = new Employee("Joe", "Bloggs");
+		System.out.println(e3);
+		System.out.println(e4);
+		System.out.println("No. of Employees: " + Employee.getCount());
+		
 		e1 = null;
 		e2 = null;
 		
 		System.gc(); // explicit call to garbage collector
+		
+		while(Employee.getCount() != 0) {
+			System.out.println("Waiting on garbage collector to finish");
+		}
 		System.out.println("Employees after garbage collection : " + Employee.getCount());
 		
 	}
