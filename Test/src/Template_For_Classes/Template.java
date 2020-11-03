218ff9c850aed78c03407874d261c914baefd97a
rasul <rasul@LAPTOP-K1VMQB3G>
2020-11-03 09:23:24 +0000

    Just created a package with Template for not writing default staff in the Future

---
+++ Test/src/Template_For_Classes/Template.java
@@ -0,0 +1,39 @@
+package Template_For_Classes;
+
+import java.util.Scanner;  //for using input by human "x = in.nextLine();'
+
+
+//			Student Name: 		Rasul Iskandarov
+
+//			Student ID : 		       C00246498
+
+//			Student Group : 	      CW_KCSOF_D
+
+public class Template {
+
+	
+	
+	//Variables
+	
+	
+	//Constructors
+	
+	
+	//Getters & Setters
+	
+		
+	//toString	
+	
+	
+	//Misc Methods
+	
+	
+	
+	
+	public static void main(String[] args) {  
+		
+		
+
+	}	//end of main
+	
+}
